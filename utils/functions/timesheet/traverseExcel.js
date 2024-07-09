fec83569f695f224bc5a63357d4640422e52327d
Brian Sai <brianwsai@gmail.com>
2024-07-08 20:58:14 -0700

    Logged error message for each row with an error

--- utils/functions/timesheet/traverseExcel.js
+++ utils/functions/timesheet/traverseExcel.js
@@ -2,11 +2,17 @@ const { calculateShiftHours } = require('./calculateShiftHours');
 const { traverseRow } = require('./traverseRow');
 
 const traverseExcel = (excel) => {
+  const arrayOfErrors = [];
   for (let x = 0; x < excel.length; x++) {
     const shiftHours = calculateShiftHours(excel[x]);
+    if (shiftHours) {
+      const matchErrors = traverseRow(excel[x], shiftHours);
 
-    shiftHours && traverseRow(excel[x], shiftHours);
+      matchErrors.length &&
+        arrayOfErrors.push({ [`row ${x + 1}`]: matchErrors });
+    }
   }
+  console.log(arrayOfErrors);
 };
 
 module.exports = { traverseExcel };
--- utils/functions/timesheet/traverseRow.js
+++ utils/functions/timesheet/traverseRow.js
@@ -1,11 +1,18 @@
 const { matchHours } = require('./matchHours');
 
 const traverseRow = (row, hours) => {
+  const errors = [];
+  const errorMessage = 'Hours do not match';
   const weekOne = row.slice(6, 13);
   const weekTwo = row.slice(14, row.length - 2);
 
-  const weekOneHours = matchHours(weekOne, hours);
-  const weekTwoHours = matchHours(weekTwo, hours);
+  const weekOneHoursCorrect = matchHours(weekOne, hours);
+  const weekTwoHoursCorrect = matchHours(weekTwo, hours);
+
+  !weekOneHoursCorrect && errors.push(`Week 1 error. ${errorMessage}`);
+  !weekTwoHoursCorrect && errors.push(`Week 2 error. ${errorMessage}`);
+
+  return errors;
 };
 
 module.exports = { traverseRow };
